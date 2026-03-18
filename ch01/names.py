bea4b2a5a525ba5be1b6316ba4969eadb6724076
Jorge Enrique Hernandez Guzman <jehernandezg@uoc.edu>
2026-03-18 10:57:18 -0500

    Add files via upload

---
+++ ch01/names.py
@@ -0,0 +1,12 @@
+n = 3  # integer number
+address = "221b Baker Street, NW1 6XE, London"  # Sherlock Holmes' address
+employee = {
+'age': 45,
+'role': 'CTO',
+'SSN': 'AB1234567',
+}
+# let us print them
+print(n,address,employee)
+
+
+
